fc7afa0862788b6c19b13b284ed33224f8427167
Ashley Rivera <ashleyariv@gmail.com>
2023-08-04 15:41:24 -0400

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer; // declaring
+const leastFavoriteCustomer = 'ash';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // assigning
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'meg';
+}
